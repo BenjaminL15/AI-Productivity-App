69d55da0a034b4a5e997c566d59bccf28674a884
YansonK <Khuuyanson@gmail.com>
2024-06-25 02:22:34 -0400

    functional chat IF ENV variables can be figured out

--- application/functions/main.py
+++ application/functions/main.py
@@ -1,25 +1,34 @@
 # Welcome to Cloud Functions for Firebase for Python!
 # To get started, simply uncomment the below code or create your own.
 # Deploy with `firebase deploy`
-
+from groq import Groq
+import os
 from firebase_functions import https_fn, options
+from firebase_functions.params import StringParam
 from firebase_admin import db, initialize_app
 import firebase_admin
 from firebase_admin import credentials
-cred = credentials.Certificate("./secret.json")
-# firebase_admin.initialize_app(cred)
 
-initialize_app()
-#
-#
-@https_fn.on_request()
-def on_request_example(req: https_fn.Request) -> https_fn.Response:
-    return https_fn.Response("hello world")
+CRED = credentials.Certificate("./secret.json")
+GROQ_API_KEY = StringParam("GROQ_API")
+# firebase_admin.initialize_app(cred)
+client = Groq(api_key=GROQ_API_KEY)
+
+app = initialize_app()
 
 @https_fn.on_call()
 def test(req: https_fn.CallableRequest) -> https_fn.Response:
     """Saves a message to the Firebase Realtime Database but sanitizes the text
     by removing swear words."""
     text = req.data["text"]
-    response = f"Response: {text} is what you sent"
-    return {"response": response}
+
+    chat_completiton = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": text,
+            }
+        ],
+        model="llama3-70b-8192"
+    )
+    return {"response": chat_completiton.choices[0].message.content}
