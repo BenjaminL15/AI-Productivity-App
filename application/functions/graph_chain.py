13ffaf1f3c21aea1d7af5ef04d410478ab7249de
Benjamin Lee <sfw7sg@virginia.edu>
2024-07-06 14:02:53 -0400

    Invoke
    Checkpoint before tooks

---
+++ application/functions/graph_chain.py
@@ -0,0 +1,85 @@
+from typing import List, Optional, TypedDict
+from langchain.output_parsers.openai_tools import JsonOutputToolsParser
+from langgraph.graph import END, StateGraph  # not sure about the from langgraph import stuff 
+from langgraph.graph.graph import CompiledGraph
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables  import RunnableConfig
+
+
+
+from langchain_groq import ChatGroq
+from firebase_functions.params import StringParam
+
+GROQ_API_KEY = StringParam("GROQ_API")
+
+def create_llm():
+    return ChatGroq(
+        temperature=0,
+        model="llama3-70b-8192",
+        api_key=GROQ_API_KEY.value
+    )
+
+ASSIGN_TOOLS = "Assigning the tools"
+RESPONSE = "Producing the response"
+MODEL_CALL = "Calling the model"
+
+class GenerativeUIState(TypedDict, total=False):
+    input: List[tuple]
+    result: Optional[str]
+    # parsed tool calls
+    tool_calls: Optional[List[dict]]
+    # result of the tool calls
+    tool_result: Optional[dict]
+
+
+def produce_response(state: GenerativeUIState, config: RunnableConfig) -> str:
+    print("we are at the produce response")
+
+    RESPONSE_PROMPT = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "You are a helpful assistant that guides users in breaking down tasks.\
+                 You make users feel heard while prompting them to think about their\
+                 task and how it can be reduced or if they have already made progress\
+                 towards said task. Keep reducing tasks until they can be completed\
+                 within a couple of minutes. Make the user feel as little overwhelmed\
+                 as possible. Ask one thing at a time to help pin point a first task\
+                 that can be completed. Be concise."
+            ),   
+        ] + state["input"]
+    )
+    model = create_llm()
+    chain = RESPONSE_PROMPT | model | StrOutputParser()
+    result = chain.invoke(config)
+    print("we are at the end of produce response")
+    return {"input" : [("system", result)] }
+
+def invoke_model(state: GenerativeUIState, config: RunnableConfig) -> str:
+    print("We are at invoke_model")
+    return 
+
+def invoke_tools(state: GenerativeUIState, config: RunnableConfig) -> str:
+    print("We are at invoke_tools")
+    return {"input" : [("system", "Mention today is Saturday")] }
+
+def create_graph() -> CompiledGraph:
+    print("We are in create graph")
+
+    workflow = StateGraph(GenerativeUIState)
+
+    # workflow.add_node(MODEL_CALL, invoke_model) # This part calls the AI and it will decide what to respond with
+    # workflow.add_node(ASSIGN_TOOLS, invoke_tools) # This basically assigns and calls the tools that the AI is assigned to or return plain text
+    workflow.add_node(MODEL_CALL, invoke_model)
+    workflow.add_node(ASSIGN_TOOLS, invoke_tools)
+    workflow.add_node(RESPONSE, produce_response) # adding the response 
+    workflow.add_edge(MODEL_CALL, ASSIGN_TOOLS)
+    workflow.add_edge(ASSIGN_TOOLS, RESPONSE)
+    # workflow.add_conditional_edges(MODEL_CALL, invoke_tools_or_respond) # If no tools assigned, it will just return a default text
+    # workflow.add_edge(ASSIGN_TOOLS, RESPONSE)
+    workflow.set_entry_point(MODEL_CALL) # starting point is the calling the model
+    workflow.set_finish_point(RESPONSE) # end point is assigning and calling the tools or the END point which is the text response 
+
+    graph = workflow.compile()
+    return graph
--- application/functions/main.py
+++ application/functions/main.py
@@ -9,38 +9,39 @@ from firebase_functions.params import StringParam
 from firebase_admin import db, initialize_app
 import firebase_admin
 from firebase_admin import credentials
+from graph_chain import create_graph
 
 CRED = credentials.Certificate("./secret.json")
-GROQ_API_KEY = StringParam("GROQ_API")
+# GROQ_API_KEY = StringParam("GROQ_API")
 # firebase_admin.initialize_app(cred)
-client = Groq(api_key=GROQ_API_KEY.value)
+# client = Groq(api_key=GROQ_API_KEY.value)
 
 app = initialize_app()
+
+def format_chat_history(raw_chat: List[Dict], input: str) -> List[tuple]:
+            """Formats the chat history to be compatible with the OpenAI chat completion API."""
+            print("We are in format chat history")
+            formatted_chat_history = []
+            for message in reversed(raw_chat):
+                text = message.get('text', '')
+                respondent = message.get('user', '')
+                formatted_chat_history.append((respondent, text))
+            formatted_chat_history.append(("user", input))
+            return formatted_chat_history
 
 @https_fn.on_call()
 def test(req: https_fn.CallableRequest) -> https_fn.Response:
     """Saves a message to the Firebase Realtime Database but sanitizes the text
     by removing swear words."""
+    print("We are in test")
     data = req.data
     chat_history: List[Dict] = data['messages']
     input = data['inputText']
-    messages_for_prompt = []
-
-    for message in reversed(chat_history):
-        text = message.get('text', '')
-        respondent = message.get('user', '')
-        
-        messages_for_prompt.append({
-            "role": respondent,
-            "content": text,
-        })
-    messages_for_prompt.append({
-        "role": 'user',
-        "content": input
-    })
-
-    chat_completiton = client.chat.completions.create(
-        messages= messages_for_prompt,
-        model= "llama3-70b-8192"
-    )
-    return {"response": chat_completiton.choices[0].message.content}
+    graph = create_graph()
+    messages_for_prompt = format_chat_history(chat_history, input)
+    print("We reached the end of this")
+    ret = graph.invoke({"input" : messages_for_prompt})
+    print("We reached the invoke")
+    
+    print(ret)
+    return {"response": ret["input"][-1][1]}
