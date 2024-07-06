9cd739763185d561d84d3e778a442e41a469d543
Benjamin Lee <sfw7sg@virginia.edu>
2024-07-05 21:35:55 -0400

    Graph stuff AI
    AI stuff gor graph chain

--- application/functions/graph_chain.py
+++ application/functions/graph_chain.py
@@ -1,11 +1,13 @@
 
 from typing import List, Optional, TypedDict
+from langchain.output_parsers.openai_tools import JsonOutputToolsParser
 from langgraph.graph import END, StateGraph  # not sure about the from langgraph import stuff 
 from langgraph.graph.graph import CompiledGraph
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables  import RunnableConfig
 from base_chains import BaseChain
+from tools.activate_task import create_actionable_task
 
 class GenerativeUIState(TypedDict, total=False):
     input: Optional[List[(str, str)]]
@@ -16,26 +18,43 @@ class GenerativeUIState(TypedDict, total=False):
     tool_result: Optional[dict]
 
 def invoke_model(state: GenerativeUIState, config: RunnableConfig) -> GenerativeUIState:
-    SYSTEM_PROMPT = "You are a helpful assistant that will read the chat history with the user and deteremine whether.\
-                    or not the messages are relevant enough to invoke a tool or respond with relevant text."
+    tools_parser = JsonOutputToolsParser()
+    INITIAL_PROMPT = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "You are a helpful assistant that will read the chat history with the user and deteremine whether.\
+                or not the messages are relevant enough to invoke a tool or respond with relevant text."
+            )
+        ] + state["input"]
+    )
 
     model = BaseChain.create_llm()
-    tools = placeholder 
-    model_with_tools = model.bind_tools(tools)
-    chain = SYSTEM_PROMPT | model_with_tools
-    result = chain.invoke({"input": state["input"]}, config)
+    model_with_tools = model.bind_tools(create_actionable_task)
+    chain = INITIAL_PROMPT | model_with_tools
+    result = chain.invoke(config)
 
     if isinstance(result.tool_calls, list) and len(result.tool_calls) > 0:
         parsed_tools = tools_parser.invoke(result, config)
         return {"tool_calls": parsed_tools}
-    else:
-        return {"tool_calls": None}
     
 def invoke_tools_or_respond(state: GenerativeUIState) -> str:
     if "tool_calls" in state and isinstance(state["tool_calls"], list):
         return "invoke_tools" 
     else:
         return "produce_response"
+    
+def invoke_tools(state: GenerativeUIState) -> GenerativeUIState:
+    tools_map = {
+        "activate-task": create_actionable_task
+    }
+
+    if state["tool_calls"] is not None:
+        tool = state["tool_calls"][0]
+        selected_tool = tools_map[tool["type"]]
+        return {"tool_result": selected_tool.invoke(tool["args"])}
+    else:
+        raise ValueError("No tool calls found in state.")
     
 def produce_response(state: GenerativeUIState, config: RunnableConfig) -> str:
     if "tool_calls" in state and isinstance(state["tool_calls"], list):
@@ -55,24 +74,24 @@ def produce_response(state: GenerativeUIState, config: RunnableConfig) -> str:
                  within a couple of minutes. Make the user feel as little overwhelmed\
                  as possible. Ask one thing at a time to help pin point a first task\
                  that can be completed. Be concise."
-            ),
-            MessagesPlaceholder("input"),
-        ]
+            ),   
+        ] + state["input"]
     )
     model = BaseChain.create_llm()
     chain = RESPONSE_PROMPT | model | StrOutputParser()
-    result = chain.invoke({"input": state["input"]}, config)
+    result = chain.invoke(config)
 
-    return result 
+    return {"input" : [("system",result)] }
 
 def create_graph() -> CompiledGraph:
 
     workflow = StateGraph(GenerativeUIState)
 
     workflow.add_Node("Calling the model", invoke_model) # This part calls the AI and it will decide what to respond with
-    workflow.add_Node("Assigning the tools", tool_placeholder) # This basically assigns and calls the tools that the AI is assigned to or return plain text
+    workflow.add_Node("Assigning the tools", invoke_tools) # This basically assigns and calls the tools that the AI is assigned to or return plain text
     workflow.add_Node("Producing the response", produce_response) # adding the response 
     workflow.add_conditional_edges("Calling model", invoke_tools_or_respond) # If no tools assigned, it will just return a default text
+    workflow.add_edge("Assigning the tools", "Producing the response")
     workflow.set_entry_point("Calling the model") # starting point is the calling the model
     workflow.set_finish_point("Producing the response") # end point is assigning and calling the tools or the END point which is the text response 
 
--- application/functions/main.py
+++ application/functions/main.py
@@ -2,6 +2,9 @@
 # To get started, simply uncomment the below code or create your own.
 # Deploy with `firebase deploy`
 
+from graph_chain import create_graph
+from typing import List, Dict
+
 from firebase_functions import https_fn, options
 from firebase_functions.params import StringParam
 from firebase_admin import db, initialize_app
@@ -19,11 +22,17 @@ SYSTEM_PROMPT = "You are a helpful assistant that guides users in breaking down 
                  as possible. Ask one thing at a time to help pin point a first task\
                  that can be completed. Be concise."
 
-# firebase_admin.initialize_app(cred)
-client = Groq(api_key=GROQ_API_KEY.value)
-
 app = initialize_app()
 
+def format_chat_history(raw_chat: List[Dict], input: str) -> List[(str, str)]:
+            """Formats the chat history to be compatible with the OpenAI chat completion API."""
+            formatted_chat_history = []
+            for message in reversed(raw_chat):
+                text = message.get('text', '')
+                respondent = message.get('user', '')
+                formatted_chat_history.append((respondent, text))
+            formatted_chat_history.append(("user", input))
+            return formatted_chat_history
 
 
 @https_fn.on_call()
@@ -33,10 +42,9 @@ def test(req: https_fn.CallableRequest) -> https_fn.Response:
     data = req.data
     chat_history: List[Dict] = data['messages']
     input = data['inputText']
+    graph = create_graph()
     messages_for_prompt = format_chat_history(chat_history, input)
-
-    chat_completiton = client.chat.completions.create(
-        messages= messages_for_prompt,
-        model= "llama3-70b-8192"
-    )
-    return {"response": chat_completiton.choices[0].message.content}
+    ret = graph.invoke(input=messages_for_prompt)
+    
+    print(ret)
+    return {"response": ret["input"][-1][1]}
